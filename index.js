88b46485a694e10d255129b8bdf058da67918ac5
chiragagicent <chirag.ramnani@agicent.in>
2024-07-03 19:02:42 +0530

    2nd

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
 import Embed from "@editorjs/embed";
-import ImageTool from "@editorjs/image";
-
+import SimpleImage from "@editorjs/simple-image";
+import CodeTool from "@editorjs/code";
+import Delimiter from "@editorjs/delimiter";
 const editor = new EditorJS({
   holderId: "editorjs",
   tools: {
@@ -23,51 +24,9 @@ const editor = new EditorJS({
         coub: true,
       },
     },
-  },
-  image: {
-    class: ImageTool,
-    config: {
-      /**
-       * Custom uploader
-       */
-      uploader: {
-        /**
-         * Upload file to the server and return an uploaded image data
-         * @param {File} file - file selected from the device or pasted by drag-n-drop
-         * @return {Promise.<{success, file: {url}}>}
-         */
-        uploadByFile(file) {
-          // your own uploading logic here
-          return MyAjax.upload(file).then(() => {
-            return {
-              success: 1,
-              file: {
-                url: "",
-                // any other image data you want to store, such as width, height, color, extension, etc
-              },
-            };
-          });
-        },
-
-        /**
-         * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
-         * @param {string} url - pasted image URL
-         * @return {Promise.<{success, file: {url}}>}
-         */
-        uploadByUrl(url) {
-          // your ajax request for uploading
-          return MyAjax.upload(file).then(() => {
-            return {
-              success: 1,
-              file: {
-                url: "",
-                // any other image data you want to store, such as width, height, color, extension, etc
-              },
-            };
-          });
-        },
-      },
-    },
+    image: SimpleImage,
+    code: CodeTool,
+    delimiter: Delimiter,
   },
 });
 
